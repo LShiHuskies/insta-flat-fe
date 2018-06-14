adfed4e9ca72a146582b29c418af9d5d163316a5
github username <github email address>
2018-06-14 14:55:10 -0400

    finished practice

--- src/index.js
+++ src/index.js
@@ -5,3 +5,96 @@
 //        <span id="likes-count-for-image-1">41</span>
 //    </p>
 //</div>
+
+
+const postImageForm = document.getElementById('post-image-form')
+const imageContainer = document.getElementById('container')
+const imageURL = 'http://localhost:3001/api/v1/images'
+const imageText = document.getElementById('post-image-form-url')
+const likesURL = 'http://localhost:3001/api/v1/likes'
+
+
+fetch(imageURL).then(r=>r.json()).then(getImages)
+
+postImageForm.addEventListener('submit', function(event){
+  event.preventDefault()
+  const input = imageText.value
+  makeImagePost(input)
+
+})
+
+
+imageContainer.addEventListener('click', function(event){
+  if (event.target.dataset.action == "like-image") {
+    let likeCount =  Number(event.target.parentElement.innerText)
+    let imageId = event.target.parentElement.children[0].dataset.imageId
+    addMoreLike(imageId, likeCount, event)
+  }
+})
+
+function getImages(images) {
+
+  images.map(getImage)
+}
+
+
+function getImage(image) {
+  // li = document.createElement('LI')
+  // li.setAttribute('id', `${image.id}`)
+  // li.innerHTML = `<img src="${image.url}"> ${image.likes_count}<button id="${image.id}"> LIKE </button>`
+  // imageContainer.appendChild(li)
+
+  imageContainer.innerHTML += `<div class="image-container">
+      <img src="${image.url}">
+     <p>
+          <img data-action="like-image" data-image-id="${image.id}" class="like-button" src="./images/like.png"><br>
+          <span id="likes-${image.id}">${image.likes_count}</span>
+      </p>
+  </div> `
+
+
+}
+
+function addMoreLike(imageId, likeCount, event) {
+  const body = {
+    image_id: imageId
+  }
+  const config = {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body:JSON.stringify(body)
+  }
+
+
+  fetch(likesURL, config).then(r => r.json()).then(a => moreLike(a, event))
+
+}
+
+
+  function moreLike(a, event) {
+
+    let likeCount = Number(event.target.parentElement.children[2].innerText)
+    likeCount = 1 + likeCount
+    event.target.parentElement.children[2].innerText = likeCount
+
+  }
+
+
+
+function makeImagePost(input) {
+  const body = {
+    url: input
+  }
+  const config = {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(body)
+  }
+
+  fetch(imageURL, config).then(r => r.json()).then(getImage)
+
+}
